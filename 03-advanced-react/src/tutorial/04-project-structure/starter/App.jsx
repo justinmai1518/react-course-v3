92508e3f0c454fe22069b2177ae9e61160197b69
Justin Mai <maingockhuong89@gmail.com>
2023-09-28 15:22:22 -0500

    Leverage Javascript

---
+++ 03-advanced-react/src/tutorial/04-project-structure/starter/App.jsx
@@ -0,0 +1,11 @@
+import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx'
+function App() {
+  return (
+    <div className="container">
+      <Starter />
+      {/* <Final /> */}
+    </div>
+  )
+}
+
+export default App
