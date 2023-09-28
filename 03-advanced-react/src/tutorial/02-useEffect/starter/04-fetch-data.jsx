5a936d9b74d7bdfd499e1c00472ff0cddd602036
Khuong Mai <maingockhuong89@gmail.com>
2023-09-27 21:32:10 -0500

    Toggle Challenge - 03

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 const url = 'https://api.github.com/users'
 
 const FetchData = () => {
-  const { users, setUsers } = useState([])
+  const [users, setUsers] = useState([])
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -18,23 +18,24 @@ const FetchData = () => {
     fetchData()
   }, [])
   return (
-    <div>
+    <section>
       <h2>fetch data example</h2>
       <ul>
-        {users.map((users) => {
-          const { id, login, avatar_url, url } = users
-          console.log(users)
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          console.log(user)
           return (
             <li key={id}>
-              <h3>
-                <a href={url}>{login}</a>
-              </h3>
               <img src={avatar_url} />
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>profile</a>
+              </div>
             </li>
           )
         })}
       </ul>
-    </div>
+    </section>
   )
 }
 export default FetchData
--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,27 @@
-const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+import { useEffect, useState } from 'react'
 
-export default CleanupFunction;
+const CleanupFunction = () => {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <>
+      <h2>cleanup function</h2>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle component
+      </button>
+      {toggle && <RandomeComponent />}
+    </>
+  )
+}
+
+const RandomeComponent = () => {
+  useEffect(() => {
+    // console.log('this is interesting')
+    const intID = setInterval(() => {
+      console.log('hello from interval')
+    }, 1000)
+    return () => clearInterval(intID)
+  }, [])
+  return <h1>Hello there</h1>
+}
+
+export default CleanupFunction
